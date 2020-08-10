06e911526bd15a5b2fc0b3fe5ea8d0cca8edc080
unknown <dj9824@126.com>
2020-08-10 18:17:00 +0800

    [danjie] feat: count types number

--- 3-object/values/index.js
+++ 3-object/values/index.js
@@ -1,3 +1,5 @@
 export default function countTypesNumber(source) {
   // TODO 6: 在这里写实现代码
+  const values = Object.values(source);
+  return values.reduce((prev, acc) => parseInt(prev, 0x10) + parseInt(acc, 0x10));
 }
