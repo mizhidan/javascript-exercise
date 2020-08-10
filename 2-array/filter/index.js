baf15a1b4a547aede875c898148d4cf333699991
unknown <dj9824@126.com>
2020-08-10 16:00:02 +0800

    [danjie] feat: choose multiples of three

--- 2-array/filter/index.js
+++ 2-array/filter/index.js
@@ -1,5 +1,7 @@
 function chooseMultiplesOfThree(collection) {
   // TODO 1: 在这里写实现代码
+  const result = collection.filter(item => item % 3 === 0);
+  return result;
 }
 
 function chooseNoRepeatNumber(collection) {
