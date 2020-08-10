ae0d36efcd6eac3779c7a2feb9d53381a9af39bf
unknown <dj9824@126.com>
2020-08-10 23:02:51 +0800

    [danjie] feat: use async await fetch

--- 5-async/4-async/index.js
+++ 5-async/4-async/index.js
@@ -1,6 +1,9 @@
 async function fetchData(url) {
   // <-- start
   // TODO 24: 通过await/async实现异步请求
+  const result = await fetch(url);
+  const jsonData = await result.json();
+  document.writeln(jsonData.name);
   // end -->
 }
 
