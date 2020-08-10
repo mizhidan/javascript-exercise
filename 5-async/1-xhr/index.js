6ba088c7e32ccb420e95e9cde2d8baada2e5eac2
unknown <dj9824@126.com>
2020-08-10 21:21:23 +0800

    [danjie] feat: xhr send

--- 5-async/1-xhr/index.js
+++ 5-async/1-xhr/index.js
@@ -2,7 +2,11 @@ function fetchData(url, successCallback, errorCallback) {
   const xhr = new XMLHttpRequest();
   // <-- start
   // TODO 21: 通过XMLHttpRequest实现异步请求
-
+  xhr.open('get', url, true);
+  xhr.onerror = function() {
+    throw new Error('failed');
+  };
+  xhr.send();
   // end -->
 }
 
