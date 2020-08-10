83fd6c49ff38fb6fb62a0927e0b2878ba93ddaeb
unknown <dj9824@126.com>
2020-08-10 18:11:22 +0800

    [danjie] feat: add serial number

--- 2-array/map/index.js
+++ 2-array/map/index.js
@@ -1,3 +1,7 @@
 export default function numberMapToWord(collection) {
   // TODO 3: 在这里写实现代码
+  const result = collection.map(item => {
+    return String.fromCharCode(96 + parseInt(item, 0x10));
+  });
+  return result;
 }
